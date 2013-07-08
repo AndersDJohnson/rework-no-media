4050b5ff3a35c4c7c6d3e45ea19b5dc745004b01
Anders D. Johnson <n/a>
2013-07-07 21:58:48 -0500

    add option to skip print media queries

--- test/test.js
+++ test/test.js
@@ -7,15 +7,42 @@ var rework = require('rework')
 function fixture(name) {
   return read('test/fixtures/' + name + '.css', 'utf8')
     .trim()
-    .replace('\r', '');
+    .replace(/\r/g, '');
 }
 
 describe('rework-no-media', function(){
-  it('should remove media queries', function() {
+
+  it('should remove general media queries', function() {
     rework(fixture('general'))
       .use(noMedia())
       .toString()
       .should.equal(fixture('general.out'));
   });
+
+  it('should skip print media queries by default', function() {
+    rework(fixture('print'))
+      .use(noMedia())
+      .toString()
+      .should.equal(fixture('print.keep.out'));
+  });
+
+  it('should skip print media queries by option', function() {
+    rework(fixture('print'))
+      .use(noMedia({
+        skipPrint: true
+      }))
+      .toString()
+      .should.equal(fixture('print.keep.out'));
+  });
+
+  it('should not skip print media queries by option', function() {
+    rework(fixture('print'))
+      .use(noMedia({
+        skipPrint: false
+      }))
+      .toString()
+      .should.equal(fixture('print.noKeep.out'));
+  });
+
 });
 
